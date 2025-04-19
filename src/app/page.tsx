68936eec383479bdbc8785fb7d71beceabfb39af
neomn110 <neomn110@gmail.com>
2025-04-19 10:27:06 +0330

    refactor: remove tailwind since im using material ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
+"use client"
 
-export default function Home() {
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import TestComponent from './components/test_component';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
+
+
+export default function App() {
   return (
     <>
-      <html>
-        
-      </html>
-      <body>
-        <h1> this is body </h1>
-      </body>
-    </>
+      <TestComponent /> 
+    </>   
   );
 }
