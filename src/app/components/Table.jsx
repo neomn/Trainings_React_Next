beadcae1671d08f0fdec153172aea1da9335cdf5
neomn110 <neomn110@gmail.com>
2025-04-25 09:35:22 +0330

    feat: add dark theme and divider

--- src/app/components/Table.jsx
+++ src/app/components/Table.jsx
@@ -8,6 +8,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Divider, Stack } from '@mui/material';
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
@@ -23,34 +24,57 @@ const rows = [
 
 export default function BasicTable() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+    <Stack direction="column" spacing={2} alignItems="center">
+      <h3>buttons place holder</h3>
+      <Divider sx={{ 
+        width: '100%', 
+        maxWidth: 1500, 
+        textAlign: 'center',
+        '&::before': {
+          borderImage: 'linear-gradient(to left,rgb(3, 201, 118), transparent)',
+          borderImageSlice: 1,
+          borderWidth: '2px'
+        },
+        '&::after': {
+          borderImage: 'linear-gradient(to right,rgb(3, 201, 118), transparent)',
+          borderImageSlice: 1,
+          borderWidth: '2px'
+        },
+        '& .MuiDivider-wrapper': {
+          padding: '0 24px',
+          color: 'rgb(3, 201, 118)',
+          fontWeight: 'bold'
+        }
+      }}>Diffs Table</Divider>
+      <TableContainer component={Paper}>
+        <Table stickyHeader sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ fontWeight: 'bold', fontSize: '1.1rem' }}>Dessert (100g serving)</TableCell>
+              <TableCell align="right" sx={{ fontWeight: 'bold', fontSize: '1.1rem' }}>Calories</TableCell>
+              <TableCell align="right" sx={{ fontWeight: 'bold', fontSize: '1.1rem' }}>Fat (g)</TableCell>
+              <TableCell align="right" sx={{ fontWeight: 'bold', fontSize: '1.1rem' }}>Carbs (g)</TableCell>
+              <TableCell align="right" sx={{ fontWeight: 'bold', fontSize: '1.1rem' }}>Protein (g)</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Stack>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 "use client";
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
 import Table from "@/app/components/Table";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export default function App() {
   return (
     <>
-      <h3>main page</h3>
-      <Table/>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <h3>main page</h3>
+        <Table/>
+      </ThemeProvider>
     </>   
   );
 }
